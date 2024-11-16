d7afec39e78d9ab5b6fe4624057adfd8e0d5ee4d
Vikram Balaji <mr.vikram.balaji@gmail.com>
2024-11-15 20:28:56 -0600

    Initial Commit

---
+++ carIdentifier/carIdentifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  carIdentifierApp.swift
+//  carIdentifier
+//
+//  Created by Vikram Balaji on 11/15/24.
+//
+
+import SwiftUI
+
+@main
+struct carIdentifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ carIdentifierTests/carIdentifierTests.swift
@@ -0,0 +1,17 @@
+//
+//  carIdentifierTests.swift
+//  carIdentifierTests
+//
+//  Created by Vikram Balaji on 11/15/24.
+//
+
+import Testing
+@testable import carIdentifier
+
+struct carIdentifierTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
